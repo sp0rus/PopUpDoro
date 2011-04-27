22886cb86dbb45cec7045c375cff5219f990492a
John Stitt <jmstitt@gmail.com>
2011-04-27 01:09:12 -0500

    Added sound to be played with each notification.

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,7 +1,7 @@
 #pomodoro work timer
-DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "New Pomodoro starts" "You have 25 minutes to work."
+DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "New Pomodoro starts" "You have 25 minutes to work."; aplay /usr/share/sounds/purple/login.wav
 # 25 minutes timer
 sleep 1500
 
 #pomodoro short break timer
-DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "Pomodoro  ends" "Take a break!"
+DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "Pomodoro  ends" "Take a break!"; aplay /usr/share/sounds/purple/login.wav
