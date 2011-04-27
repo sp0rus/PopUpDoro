1b24e2962c00e934a1cf3d52e12f2b74bc0635cc
John Stitt <jmstitt@gmail.com>
2011-04-27 00:29:48 -0500

    Initial Commit with original script.

---
+++ pomodoro.sh
@@ -0,0 +1,4 @@
+DISPLAY=:0 notify-send -t 1000 -i /home/ashfame/Dropbox/Ubuntu/icons/pomodoro.png "New Pomodoro starts" "You have 25 minutes to work."
+# 25 minutes timer
+sleep 1500
+DISPLAY=:0 notify-send -t 1000 -i /home/ashfame/Dropbox/Ubuntu/icons/pomodoro.png "Pomodoro  ends" "Take a break!"
