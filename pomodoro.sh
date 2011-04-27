5cc798e71f7fec8975bba3f099e4c630ae8d89df
John Stitt <jmstitt@gmail.com>
2011-04-27 00:42:17 -0500

    Updated pomodoro.shand added a README.

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,4 +1,7 @@
-DISPLAY=:0 notify-send -t 1000 -i /home/ashfame/Dropbox/Ubuntu/icons/pomodoro.png "New Pomodoro starts" "You have 25 minutes to work."
+#pomodoro work timer
+DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "New Pomodoro starts" "You have 25 minutes to work."
 # 25 minutes timer
 sleep 1500
-DISPLAY=:0 notify-send -t 1000 -i /home/ashfame/Dropbox/Ubuntu/icons/pomodoro.png "Pomodoro  ends" "Take a break!"
+
+#pomodoro short break timer
+DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "Pomodoro  ends" "Take a break!"
