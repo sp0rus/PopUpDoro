5658c6be700665c64143d11564b9191a964da67e
John Stitt <jmstitt@gmail.com>
2011-04-27 11:34:53 -0500

    Increased readability by breaking up lines, also added a while loop to do four pomodoros then a long break.

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,7 +1,40 @@
-#pomodoro work timer
-DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "New Pomodoro starts" "You have 25 minutes to work."; aplay /usr/share/sounds/purple/login.wav
-# 25 minutes timer
-sleep 1500
+#!/bin/bash
 
-#pomodoro short break timer
-DISPLAY=:0 notify-send -t 1000 -i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png "Pomodoro  ends" "Take a break!"; aplay /usr/share/sounds/purple/login.wav
+COUNTER=0
+WORKNUMBER=4
+
+while [ "$COUNTER" -lt "$WORKNUMBER" ]
+do
+	#pomodoro work timer
+	DISPLAY=:0 notify-send \
+		-t 1000 \
+		-i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png \
+		"New Pomodoro starts" \
+		"You have 25 minutes to work." \
+		; aplay /usr/share/sounds/purple/login.wav
+	# 25 minutes until break
+	sleep 1500
+
+	let "COUNTER += 1"
+
+	#pomodoro short break timer
+	DISPLAY=:0 notify-send \
+		-t 1000 \
+		-i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png \
+		"Pomodoro ends" \
+		"Take a break!" \
+		; aplay /usr/share/sounds/purple/login.wav
+	#5 minutes for short break
+	sleep 300
+
+done
+
+#pomodoro long break
+	DISPLAY=:0 notify-send \
+		-t 1000 \
+		-i /home/sp0rus/Code/Bash/pomodoro/pomodoro.png \
+		"That's Four Pomodoros!" \
+		"Take a long break!" \
+		; aplay /usr/share/sounds/purple/login.wav
+	#15 minutes for short break
+	sleep 900
